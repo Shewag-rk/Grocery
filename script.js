d0e81be2e5d03fc27b6d8b74225693b58e11e14e
shewag-rk <shewagsurya02gmail.com>
2025-04-06 19:39:45 +0530

    delete completed

--- script.js
+++ script.js
@@ -1,25 +1,106 @@
+// products class
+class itemDetails {
+    constructor(itemName){
+        this.itemName = itemName
+    }
+
+    getItemName(){
+        return this.itemName
+    }
+}
+
+
+// Grocery class
+class Grocery {
+    constructor(){
+        this.productItems = []
+    }
+
+    addProducts(item){
+        this.productItems.push({"product": item})
+    }
+
+    getProduct(){
+        return this.productItems
+    }
+
+    editItem(index, itemName){
+        if(index >= 0 && index < this.productItems.length){
+            this.productItems[index].product = itemName
+        }
+    }
+
+    deleteItem(index){
+        if(index >= 0 && index < this.productItems.length){
+            this.productItems.splice(index, 1)
+        }
+    }
+}
+
+// Script start here:
+
+// creating instances here
 const store = new Grocery()
-const formEl = document.querySelector('form') 
-const addItem = document.querySelector('#addItem');
+const formEl = document.querySelector('form');
 
-console.log(store);
-
-
-
-
-
-addItem.addEventListener('click', function(e){
+formEl.addEventListener('submit', (e) =>{
     e.preventDefault()
 
-    const groceryItem = document.querySelector('#grocery').value.trim();
-    const groceryPrice = document.querySelector('#price').value.trim();
-    console.log(groceryItem);
-    console.log(groceryPrice);
+    const productName = document.getElementById('itemName').value.trim();
+    // console.log(productName);
 
-    store.addItem({"item":this.groceryItem, "price": this.groceryPrice})
-    console.log(store);
+    // checking the user enter the value?
+    if(!productName){
+        alert('Must enter product')
+        return;
+    }
+    
+    const addedProduct = new itemDetails(productName)
+
+    // adding products to class:
+    store.addProducts(addedProduct)
+    
+    // console.log(store);
+    appendProduct()
+
+    // removing the previous products:
+    document.getElementById('itemName').value = ''
     
 })
 
 
+function appendProduct(){
+    const productsListItem = document.querySelector('#productsListItem');
 
+    // make empty by appending new values
+    productsListItem.innerHTML = ''
+
+    store.getProduct().forEach((item, index) => {
+        
+        // console.log(item.product.getItemName());
+
+        productsListItem.innerHTML += `
+        <li>${item.product.getItemName()}</li>
+        <button class="editbtn" onclick="edit(${index})">Edit</button>
+        <button class="deletebtn" onclick="dele(${index})">Delete</button>
+        `
+    });
+}
+
+function dele(index){  
+    console.log(index);
+    store.deleteItem(index)
+    appendProduct()   
+}
+
+function edit(index){
+    const currentName = store.getProduct()[index].product.getItemName()
+    console.log(currentName);
+    let newItem = document.getElementById('itemName').value = currentName;
+
+    if(newItem.trim() == ""){
+        alert('Update the name!')
+        store.editItem(index,  newItem);
+        return
+    }
+}
